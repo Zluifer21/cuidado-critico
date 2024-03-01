5c99871aee3d7a3a6eff26d7ff79b55e74a7b5b4
Luis Zapata <hdgomez0@gmail.com>
2024-02-29 21:48:18 -0500

    send email on status change

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,6 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ public_path('img/uci.jpg') }}" class="logo" alt="UCI Logo">
-@else
-{{ $slot }}
-@endif
-</a>
-</td>
+    <td class="header">
+        <img src="https://api.cuidadocritico.com.co/img/uci.jpg" alt="UCI Logo" width="220" height="75">
+    </td>
 </tr>
